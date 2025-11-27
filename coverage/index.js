49845ed5e5153f2ee74311e5636c26a03dbfece3
apotema <apotema@users.noreply.github.com>
2025-11-27 16:52:27 +0000

    deploy: e32b6b1bff927daa3b9553e7bb8096fb5632ea4b

---
+++ coverage/index.js
@@ -0,0 +1,8 @@
+var data = {files:[
+{"link":"matchers.zig.dae5ec5d.html","title":"matchers.zig","summary_name":"[...]/zspec/src/matchers.zig","covered_class":"linePartCov","covered":"74.9","covered_lines":"128","uncovered_lines":"43","total_lines" : "171"},
+{"link":"factory.zig.6fe16dde.html","title":"factory.zig","summary_name":"[...]/zspec/src/factory.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"118","uncovered_lines":"0","total_lines" : "118"},
+{"link":"zspec.zig.d1596f30.html","title":"zspec.zig","summary_name":"[...]/zspec/src/zspec.zig","covered_class":"lineCov","covered":"91.7","covered_lines":"55","uncovered_lines":"5","total_lines" : "60"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "test", "date" : "2025-11-27 16:52:24", "instrumented" : 349, "covered" : 301,};
+var merged_data = [];
